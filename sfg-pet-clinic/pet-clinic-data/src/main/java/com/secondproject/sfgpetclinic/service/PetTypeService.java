83057b4071e79fc5503b6b324cfa3fb5003bc628
rg1107 <rahulg.171co132@nitk.edu.in>
2021-01-15 15:23:03 +0530

    Added PetType Map Impl.Closes #29

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/secondproject/sfgpetclinic/service/PetTypeService.java
@@ -0,0 +1,8 @@
+package com.secondproject.sfgpetclinic.service;
+
+import com.secondproject.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long>  {
+	
+	
+}
---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/secondproject/sfgpetclinic/service/map/PetTypeMapService.java
@@ -0,0 +1,45 @@
+package com.secondproject.sfgpetclinic.service.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import com.secondproject.sfgpetclinic.model.PetType;
+import com.secondproject.sfgpetclinic.service.PetTypeService;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType,Long> implements PetTypeService {
+
+	@Override
+	public Set<PetType> findAll() {
+		// TODO Auto-generated method stub
+		return super.findAll();
+	}
+
+	@Override
+	public PetType findById(Long id) {
+		// TODO Auto-generated method stub
+		return super.findById(id);
+	}
+
+	@Override
+	public PetType save(PetType object) {
+		// TODO Auto-generated method stub
+		return super.save(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(PetType object) {
+		// TODO Auto-generated method stub
+		super.delete(object);
+	}
+	
+	
+	
+}
