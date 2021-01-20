93d681f75e8aaf3ab0bd19b0961823ab60fcebde
rg1107 <rahulg.171co132@nitk.edu.in>
2021-01-20 23:36:03 +0530

    Added Project Lombok and Refactored the application. Closes #51

--- sfg-pet-clinic/pet-clinic-data/src/main/java/com/secondproject/sfgpetclinic/model/Vet.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/secondproject/sfgpetclinic/model/Vet.java
@@ -11,6 +11,17 @@ import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "vets")
 public class Vet extends Person {
@@ -20,13 +31,5 @@ public class Vet extends Person {
 			inverseJoinColumns = @JoinColumn(name = "speciality_id"))
 	private Set<Speciality> specialities = new HashSet<>();
 
-	public Set<Speciality> getSpecialities() {
-		return specialities;
-	}
-
-	public void setSpecialities(Set<Speciality> specialities) {
-		this.specialities = specialities;
-	}
-	
 
 }
