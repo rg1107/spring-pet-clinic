cb3c098720d670a5438c8a467923c114215c2161
rg1107 <rahulg.171co132@nitk.edu.in>
2021-03-03 16:15:31 +0530

    Added Functionality to Add or update a pet.Closes #56

---
+++ pet-clinic-data/src/main/java/com/secondproject/sfgpetclinic/formatters/PetTypeFormatter.java
@@ -0,0 +1,39 @@
+package com.secondproject.sfgpetclinic.formatters;
+
+import java.text.ParseException;
+import java.util.Collection;
+import java.util.Locale;
+
+import org.springframework.format.Formatter;
+import org.springframework.stereotype.Component;
+
+import com.secondproject.sfgpetclinic.model.PetType;
+import com.secondproject.sfgpetclinic.service.PetTypeService;
+
+@Component
+public class PetTypeFormatter implements Formatter<PetType> {
+
+    private final PetTypeService petTypeService;
+
+    public PetTypeFormatter(PetTypeService petTypeService) {
+        this.petTypeService = petTypeService;
+    }
+
+    @Override
+    public String print(PetType petType, Locale locale) {
+        return petType.getName();
+    }
+
+    @Override
+    public PetType parse(String text, Locale locale) throws ParseException {
+        Collection<PetType> findPetTypes = petTypeService.findAll();
+
+        for (PetType type : findPetTypes) {
+            if (type.getName().equals(text)) {
+                return type;
+            }
+        }
+
+        throw new ParseException("type not found: " + text, 0);
+    }
+}
--- pet-clinic-data/src/main/java/com/secondproject/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/secondproject/sfgpetclinic/model/Pet.java
@@ -23,10 +23,19 @@ import lombok.Setter;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "pets")
 public class Pet extends BaseEntity{
+	
+	@Builder
+    public Pet(Long id, String name, PetType petType, Owner owner, LocalDate birthDate, Set<Visit> visits) {
+        super(id);
+        this.name = name;
+        this.petType = petType;
+        this.owner = owner;
+        this.birthDate = birthDate;
+        this.visits = visits;
+    }
 	
 	@Column(name = "name")
 	private String name;
@@ -44,5 +53,7 @@ public class Pet extends BaseEntity{
 	
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
 	private Set<Visit> visits = new HashSet<>(); 
+	
+	
 
 }
